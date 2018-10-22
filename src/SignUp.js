91bd5fc13669b0ae0ef5a92dec9c9c10180b948e
Julien <julien.bertocchini@gmail.com>
2018-10-22 11:32:25 +0200

    quete React 020 formulaire d'enregistrement

--- src/SignUp.js
+++ src/SignUp.js
@@ -4,31 +4,38 @@ export default class SignUp extends Component {
     constructor(props){
         super(props);
             this.state = {
-                email: "",
+                email: "mon@email.com",
+                password: "monPassw0rd",
+                name: "James",
+                lastname: "Bond"
                 
             }
    this.updateEmail = this.updateEmail.bind(this);
-   this.submitEmail = this.submitEmail.bind(this);
+   this.handdleSubmit = this.handdleSubmit.bind(this);
               
     }
     updateEmail(event){
        
-        this.setState({email : event.target.value});
+        this.setState({[event.target.name]: event.target.value});
     }
 
-    submitEmail(event){
-
-        alert('Hey ! your e-mail : ' + this.state.email + ' was submitted');
-         event.preventDefault();
+    handdleSubmit(event){
+        event.preventDefault();
+        console.log(this.state)
+        alert("Ce message va s'auto-détruire");
+         
 
     }
     
 
   render() {
     return (
-      <form onSubmit={this.submitEmail}>
-         <h1>{this.state.email || "Enter your mail"}</h1>
+      <form onSubmit={this.handdleSubmit}>
+         <h1>{JSON.stringify(this.state,1,1)}</h1>
          <  input type="email" name="email" placeholder="e-mail here" autoComplete="off" value={this.state.email} onChange= {this.updateEmail}/>
+         <  input type="password" name="password" placeholder="password" autoComplete="off" value={this.state.password} onChange= {this.updateEmail}/>
+         <  input type="text" name="name" placeholder="Name" autoComplete="off" value={this.state.name} onChange= {this.updateEmail}/>
+         <  input type="text" name="lastName" placeholder="Last Name" autoComplete="off" value={this.state.lastname} onChange= {this.updateEmail}/>
          <input type="submit" value="Submit" />
       </form>
     )
